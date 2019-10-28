348a92974b12edb07f3e12b134fc62d78f1ebb0c
Eduardo <torrespe@mail.lcc.edu>
2019-10-27 23:10:55 -0400

    added new migration

---
+++ BidMyMechanic.Entities/Migrations/20191028031013_latestMig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BidMyMechanic.Entities.Migrations
+{
+    public partial class latestMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
